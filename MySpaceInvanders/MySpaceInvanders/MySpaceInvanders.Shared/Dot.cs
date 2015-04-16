cf3306a17bf2c0e9457619517e6eca8a65d2ef7c
Tattao <shihantao5@hotmail.com>
2015-04-16 17:28:51 +0800

    first submission

---
+++ MySpaceInvanders/MySpaceInvanders/MySpaceInvanders.Shared/Dot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Foundation;
+//using Windows.UI.Xaml.Shapes;
+
+namespace MySpaceInvanders
+{
+    class Dot
+    {
+        public Windows.UI.Xaml.Shapes.Ellipse Shape{get;set;}
+        public Point Velocity{get;set;}
+    }
+}
